7371f487a578214e90851d915a500d543a446ed8
1070841863 <1070841863@qq.com>
2019-07-03 20:46:46 +0800

    bos项目同步到github上

---
+++ src/main/java/com/bos/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.bos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author study
+ * @create 2019-07-03 20:03
+ */
+@Controller
+@RequestMapping("/index")
+public class UserController {
+
+    @ResponseBody
+    @RequestMapping("/test")
+    public String test()
+    {
+        return "111";
+    }
+}
